65e7fcead06de10c59acaf0c3639043dae026c23
jmacalupur <macalupu.j@gmail.com>
2018-04-20 18:53:05 -0500

    index, sidebar and footer done

--- index.php
+++ index.php
@@ -15,11 +15,45 @@
 		</div>
 		<div class="row">
 				<div class="col-md-8">
-					Content
+					<div class="blog-post">
+						<h2>Sample title</h2>
+						<p>Apr 20, 2018 <a href="">Alex</a></p>
+						<div class="blog-post-image">
+							<img src="images/atronauta.jpg" alt="">
+						</div>
+						<div class="blog-post-content">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</div>
+					</div>
+					<div class="blog-post">
+						<h2>Sample title</h2>
+						<p>Apr 20, 2018 <a href="">Alex</a></p>
+						<div class="blog-post-image">
+							<img src="images/atronauta.jpg" alt="">
+						</div>
+						<div class="blog-post-content">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</div>
+					</div>
+					<div class="blog-post">
+						<h2>Sample title</h2>
+						<p>Apr 20, 2018 <a href="">Alex</a></p>
+						<div class="blog-post-image">
+							<img src="images/atronauta.jpg" alt="">
+						</div>
+						<div class="blog-post-content">
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						</div>
+					</div>
 				</div>
 				<div class="col-md-4">
 					Sidebar
 				</div>
+		</div>
+		<div class="row">
+			<footer>
+				This is a footer
+			</footer>
 		</div>
 	</div>
 </body>
